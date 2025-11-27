25ce33e3b6b2ae01730abdd9a5decbb5a7ab3d66
Yashwanth M <yashwanthm0987@gmail.com>
2025-11-27 14:45:33 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11.8 MB (47.58%)

--- leetcode-solutions/0027-remove-element/solution.cpp
+++ leetcode-solutions/0027-remove-element/solution.cpp
@@ -1,17 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int k = 0;
+        int k = 0; // position to place the next valid element
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] != val) {
-                nums[k++] = nums[i];
+                nums[k] = nums[i];
+                k++;
             }
         }
         return k;
     }
 };
-
 
