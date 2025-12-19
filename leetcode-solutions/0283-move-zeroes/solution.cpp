fe224fb641a3a36d1422029582b0f7c991834173
Yashwanth M <yashwanthm0987@gmail.com>
2025-12-19 20:26:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 24 MB (18.44%)

---
+++ leetcode-solutions/0283-move-zeroes/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;                            // position to place next non-zero
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[j], nums[i]);       // move non-zero forward
+                j++;                          // only advance j now
+            }
+        }
+        // no need for a second pass; swapping already pushes zeros to the end
+    }
+};
