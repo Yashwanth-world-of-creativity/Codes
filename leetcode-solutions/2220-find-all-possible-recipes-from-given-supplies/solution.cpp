84f6797282a3cececcd9f48e4577931a67cfcce3
Yashwanth M <yashwanthm0987@gmail.com>
2026-02-18 06:53:36 +0000

    [LeetCode Sync] Runtime - 25 ms (98.48%), Memory - 101.4 MB (86.08%)

---
+++ leetcode-solutions/2220-find-all-possible-recipes-from-given-supplies/solution.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<string> findAllRecipes(vector<string>& recipes,
+                                  vector<vector<string>>& ingredients,
+                                  vector<string>& supplies) {
+        
+        int n = recipes.size();
+        
+        unordered_map<string, vector<int>> adj;
+        unordered_set<string> st(begin(supplies), end(supplies));
+        vector<int> indegree(n, 0);
+
+        // Build graph
+        for (int i = 0; i < n; i++) {
+            for (string& ing : ingredients[i]) {
+                if (!st.count(ing)) {
+                    adj[ing].push_back(i);
+                    indegree[i]++;
+                }
+            }
+        }
+
+        queue<int> que;
+
+        // Push recipes with indegree 0
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                que.push(i);
+            }
+        }
+
+        vector<string> result;
+
+        // Kahn's Algorithm
+        while (!que.empty()) {
+            int i = que.front();
+            que.pop();
+
+            string recipe = recipes[i];
+            result.push_back(recipe);
+
+            for (int& idx : adj[recipe]) {
+                indegree[idx]--;
+                if (indegree[idx] == 0)
+                    que.push(idx);
+            }
+        }
+
+        return result;
+    }
+};
