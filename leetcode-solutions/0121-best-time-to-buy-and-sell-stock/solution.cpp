52655845aa35e0e752455114da0a8e2b3ab1fdbf
Yashwanth M <yashwanthm0987@gmail.com>
2026-02-21 12:29:19 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 97.3 MB (86.75%)

--- leetcode-solutions/0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ leetcode-solutions/0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -2,14 +2,35 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        int bestbuy=prices[0];
-        int mp=0;
+        int buyPrice=INT_MAX;
+        int maxPrice=0;
         for(int i=0;i<n;i++){
-            if(prices[i]>bestbuy){
-                mp=max(mp,prices[i]-bestbuy);
+            
+            if(buyPrice<prices[i]){
+                int profit=prices[i]-buyPrice;
+                maxPrice=max(profit,maxPrice);
             }
-            bestbuy=min(bestbuy,prices[i]);
+            else{
+                buyPrice=prices[i];
+            }
         }
-        return mp;
+        return maxPrice;
     }
 };
+
+
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+//         int n=prices.size();
+//         int bestbuy=prices[0];
+//         int mp=0;
+//         for(int i=0;i<n;i++){
+//             if(prices[i]>bestbuy){
+//                 mp=max(mp,prices[i]-bestbuy);
+//             }
+//             bestbuy=min(bestbuy,prices[i]);
+//         }
+//         return mp;
+//     }
+// };
