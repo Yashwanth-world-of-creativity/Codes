e757b44e4bd09074721b9a0761ed1e24bcd1e7db
Yashwanth M <yashwanthm0987@gmail.com>
2026-03-02 19:41:27 +0000

    [LeetCode Sync] Runtime - 4 ms (20.71%), Memory - 25.1 MB (61.00%)

---
+++ leetcode-solutions/3324-split-the-array/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPossibleToSplit(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(auto i :nums){
+            mp[i]++;
+        }
+        for(auto it:mp){
+            if(it.second>2) return false;
+        }
+        return true;
+    }
+};
