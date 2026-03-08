9d1f23a713d94dd2189aa9aaa4c6461d5cd9a2b6
Yashwanth M <yashwanthm0987@gmail.com>
2026-03-08 20:38:40 +0000

    [LeetCode Sync] Runtime - 427 ms (10.25%), Memory - 150 MB (34.40%)

---
+++ leetcode-solutions/0905-length-of-longest-fibonacci-subsequence/solution.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+
+    int solve(int j, int k, vector<int>& arr, 
+              unordered_map<int,int>& mp, 
+              vector<vector<int>>& dp) {
+
+        if(dp[j][k] != -1)
+            return dp[j][k];
+
+        int target = arr[k] - arr[j];
+
+        if(mp.count(target) && mp[target] < j) {
+
+            int i = mp[target];
+
+            return dp[j][k] = solve(i, j, arr, mp, dp) + 1;
+        }
+
+        return dp[j][k] = 2; // minimum fibonacci length
+    }
+
+    int lenLongestFibSubseq(vector<int>& arr) {
+
+        int n = arr.size();
+
+        unordered_map<int,int> mp;
+
+        for(int i = 0; i < n; i++)
+            mp[arr[i]] = i;
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        int max_length = 0;
+
+        for(int j = 1; j < n; j++) {
+            for(int k = j + 1; k < n; k++) {
+
+                int length = solve(j, k, arr, mp, dp);
+
+                if(length >= 3)
+                    max_length = max(max_length, length);
+            }
+        }
+
+        return max_length;
+    }
+};
