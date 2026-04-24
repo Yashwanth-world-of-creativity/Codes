184afb3ca5d8fe2068a9cb947310ff949eb300af
Yashwanth M <yashwanthm0987@gmail.com>
2026-04-24 16:32:15 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9.1 MB (63.66%)

---
+++ leetcode-solutions/3019-furthest-point-from-origin/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int furthestDistanceFromOrigin(string moves) {
+        int left=0;
+        int right=0;
+        int dash=0;
+        for(char &c:moves){
+            if(c=='L') left++;
+            else if(c=='R') right++;
+            else dash++;
+        }
+        return abs(left-right)+dash;
+    }
+};
