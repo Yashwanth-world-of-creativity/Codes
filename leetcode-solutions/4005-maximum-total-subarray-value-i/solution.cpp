51db6ae10acc20a5945f89ef7ea5a3981e9d9176
Yashwanth M <yashwanthm0987@gmail.com>
2026-06-09 15:42:28 +0000

    [LeetCode Sync] Runtime - 3 ms (35.80%), Memory - 103.6 MB (97.35%)

---
+++ leetcode-solutions/4005-maximum-total-subarray-value-i/solution.cpp
@@ -0,0 +1,14 @@
+// class Solution {
+// public:
+//     long long maxTotalValue(vector<int>& nums, int k) {
+        
+//     }
+// };
+class Solution {
+public:
+    long long maxTotalValue(vector<int>& nums, int k) {
+        int maxVal = *max_element(nums.begin(), nums.end());
+        int minVal = *min_element(nums.begin(), nums.end());
+        return (long long)(maxVal - minVal) * k;
+    }
+};
