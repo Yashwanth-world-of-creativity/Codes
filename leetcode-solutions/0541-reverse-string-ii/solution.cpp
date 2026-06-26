4ffd4df979ebf7b56bb5a771af7dfa0cb30ec61b
Yashwanth M <yashwanthm0987@gmail.com>
2026-06-26 18:13:13 +0000

    [LeetCode Sync] Runtime - 1 ms (9.11%), Memory - 9.5 MB (76.42%)

---
+++ leetcode-solutions/0541-reverse-string-ii/solution.cpp
@@ -0,0 +1,19 @@
+// class Solution {
+// public:
+//     string reverseStr(string s, int k) {
+        
+//     }
+// };
+//op
+class Solution {
+public:
+    string reverseStr(string s, int k) {
+
+        for (int i = 0; i < s.size(); i += 2 * k) {
+            reverse(s.begin() + i,
+                    s.begin() + min(i + k, (int)s.size()));
+        }
+
+        return s;
+    }
+};
