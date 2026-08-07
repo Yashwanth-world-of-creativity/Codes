02e5f040f16d2797757e8888f3af9237802c4c7a
Yashwanth M <yashwanthm0987@gmail.com>
2026-08-07 18:28:38 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.5 MB (10.62%)

---
+++ leetcode-solutions/2269-count-elements-with-strictly-smaller-and-greater-elements-/solution.cpp
@@ -0,0 +1,23 @@
+// class Solution {
+// public:
+//     int countElements(vector<int>& nums) {
+        
+//     }
+// };
+class Solution {
+public:
+    int countElements(vector<int>& nums) {
+
+        int mn = *min_element(nums.begin(), nums.end());
+        int mx = *max_element(nums.begin(), nums.end());
+
+        int count = 0;
+
+        for (int x : nums) {
+            if (x > mn && x < mx)
+                count++;
+        }
+
+        return count;
+    }
+};
